a2aba59d26f50c063b8923bee4d687dfadc694ad
rockstar0711 <happpybopy123@outlook.com>
2023-03-02 17:34:33 -0800

    inital project

---
+++ App.js
@@ -0,0 +1,22 @@
+// import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, StatusBar } from "react-native";
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle={"dark-content"} translucent={false} />
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
